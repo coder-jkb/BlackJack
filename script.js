948aa3093ef12547d8465e10d5a81969222eee62
coder-jkb <jkb17codes@gmail.com>
2023-07-25 18:31:03 +0530

    woking on auto play

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ let prefix = ["ace_of_","2_of_","3_of_","4_of_","5_of_","6_of_","7_of_",
 let player_type = "";
 let ext = '.png'
 let selected = [];
-let player_cards = []
-let dealer_cards = []
+let player_cards = [];
+let dealer_cards = [];
 let player_total = 0; 
 let dealer_total = 0;
 let player_balance = 1000;
-let deck = {Heart:[],Spade:[],Club:[],Diamond:[]}
+let deck = {Heart:[],Spade:[],Club:[],Diamond:[]};
 let green =" rgba(0, 128, 0, 0.7)";
 let darkgreen = "rgba(0, 100, 0, 0.7)";
 let bet = 100;
+let game_count = 0;
+
+const iters = 500;
+
+let wins = { num_games: {player: 0, dealer: 0, draw: 0}, 
+             amount:{player: 0, dealer: 0} };
 
 document.getElementById("balance-amt").textContent = player_balance;
 
@@ -47,10 +53,17 @@ function insertImage(card, parent) {
     parent.appendChild(img);
 }
 
+// min and max inclusive
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function canAutoPlay() {
+  return player_type == "AI" &&player_balance >= 50 &&game_count <= iters &&
+      !document.getElementById("replay-btn").disabled &&
+      document.getElementById("AutoPlayAI").checked;
 }
 
 function get_random_card(){
@@ -61,7 +74,7 @@ function get_random_card(){
         break;
       }
     }
-    selected[selected.length] = card;
+    selected.push(card);
   
     return card
 }
@@ -79,11 +92,16 @@ function distribute_cards(){
   insertImage(card2, player);
   insertImage(card3, dealer);
   player_total = card1.val + card2.val;
-  dealer_total = card3.val
+  dealer_total = card3.val;
   player_cards = [card1, card2];
   dealer_cards = [card3];
   document.getElementById("player-total").textContent = player_total;
   document.getElementById("dealer-total").textContent = dealer_total;
+
+  // if (player_total >= 21) {
+  //   document.getElementById("player-hit").disabled = true;
+  // }
+
   checkPlayerScore();
 }
 
@@ -91,14 +109,13 @@ function hitPlayer() {
   player = document.getElementById('player-cards');
   card = get_random_card();
   insertImage(card, player);
-  player_cards[player_cards.length] = card;
+  player_cards.push(card);
   updatePlayer(card)
 }
 
 function missPlayer() {
   document.getElementById("player-hit").disabled = true;
   document.getElementById("player-miss").disabled = true;
-  // document.getElementById("dealer-hit").disabled = false;
   document.getElementById("player").style.backgroundColor = darkgreen;
   document.getElementById("dealer").style.backgroundColor = green;
   document.getElementById("turn").textContent = "Dealer's turn";
@@ -109,12 +126,11 @@ function hitDealer() {
   dealer = document.getElementById("dealer-cards");
   card = get_random_card();
   insertImage(card, dealer);
-  dealer_cards[dealer_cards.length] = card;
+  dealer_cards.push(card);
   updateDealer(card);  
 }
 
 function checkPlayerScore() {
-  // console.log(`checking player score ${player_total}`);
   if (player_total == 21) {
     document.getElementById("player-hit").disabled = true;
     document.getElementById("player-miss").disabled = true;
@@ -127,17 +143,29 @@ function checkPlayerScore() {
     document.getElementById("player-miss").disabled = true;
     document.getElementById("player").style.backgroundColor = darkgreen;
     document.getElementById("dealer").style.backgroundColor = green;
-    // console.log(`Player BUST! Dealer WON $${bet}!!`);
-    document.getElementById("turn").textContent = `Player BUST! Dealer WON $${bet}!!`;
-    document.getElementById("turn-div").style.backgroundColor = "yellow";
-    document.getElementById("replay-btn").style.visibility = "visible";
-    // if (player_balance < 50) {
-    //   document.getElementById("replay-btn").disabled = true;
-    // }
+
+    getWinner();
+    // conditions for AI auto play
+    if (canAutoPlay()) {
+      setTimeout(() => {
+        replay();
+      }, 1000);
+
+      // setTimeout(() => {
+      //   placeBet();
+      // }, 1200);
+
+      if (game_count % 10 == 0) {
+        console.log(`After ${game_count} games`);
+        console.log(">> Wins: ", wins);
+        console.log(">> Player Balance: ", player_balance);
+      }
+    }
   }
 }
 
 function updatePlayer(card) {
+
   let num_aces = 0;
   player_total = 0;
   player_cards.forEach((card) => {
@@ -201,49 +229,100 @@ function updateDealer(card) {
   document.getElementById("dealer-total").textContent = dealer_total;
 }
 
+function getWinner() {
+  
+  let winner = '';
+  if (player_total == dealer_total) {
+    document.getElementById("turn").textContent = "It's a DRAW !!!";
+    winner = "draw";
+    player_balance = player_balance + bet;
+    document.getElementById("balance-amt").textContent = player_balance;
+  } else if (player_total > 21) {
+    document.getElementById("turn").textContent = `Player BUST! Dealer WON $${bet}!!`;
+    winner = "dealer";
+  } else if (player_total == 21) {
+    document.getElementById(
+      "turn"
+    ).textContent = `Player got a Black Jack. Player WON $${bet}!!`;
+    winner = "player";
+    player_balance += 2 * bet;
+    document.getElementById("balance-amt").textContent = player_balance;
+  } else if (dealer_total == 21) {
+    document.getElementById(
+      "turn"
+    ).textContent = `Dealer got a Black Jack. Dealer WON $${bet}!!`;
+    winner = "player";
+  } else if (dealer_total > 21) {
+    document.getElementById(
+      "turn"
+    ).textContent = `Dealer BUST! Player WON $${bet}!!`;
+    winner = "player";
+    player_balance += 2 * bet;
+    document.getElementById("balance-amt").textContent = player_balance;
+  } else if (player_total > dealer_total) {
+    document.getElementById("turn").textContent = `Player WON $${bet}!!`;
+    winner = "player";
+    player_balance += 2 * bet;
+    document.getElementById("balance-amt").textContent = player_balance;
+  } else {
+    document.getElementById("turn").textContent = `Dealer WON $${bet}!!`;
+    winner = "dealer";
+  }
+
+  
+  game_count+=1;
+  console.log("Game count: ",game_count);
+
+  if (winner=="player"){
+    wins.num_games.player += 1;
+    wins.amount.player += bet;
+  }
+  else if (winner=="dealer"){
+    wins.num_games.dealer += 1;
+    wins.amount.dealer += bet;
+  }
+  else{
+    wins.num_games.draw += 1;
+  }
+
+  document.getElementById("turn-div").style.backgroundColor = "yellow";
+  document.getElementById("replay-btn").style.visibility = "visible";
+
+  // player_cards = [];
+  // dealer_cards = [];
+  
+  // return winner;
+}
+
 function dealerPlays() {
   while (dealer_total <= 17) {
     hitDealer();
   }
   // dealer's turn is over, Now its decision time
-  if (player_total == dealer_total) {
-    document.getElementById("turn").textContent = "It's a DRAW !!!";
-    player_balance = player_balance + Number(bet);
-    document.getElementById("balance-amt").textContent = player_balance;
-  } else if (player_total == 21) {
-    document.getElementById("turn").textContent = `Player got a Black Jack. Player WON $${bet}!!`;
-    player_balance += 2 * bet;
-    document.getElementById("balance-amt").textContent = player_balance;
+  getWinner();
 
-  } else if (dealer_total == 21) {
-    document.getElementById(
-      "turn"
-    ).textContent = `Dealer got a Black Jack. Dealer WON $${bet}!!`;
-  } else if (dealer_total > 21) {
-    document.getElementById("turn").textContent = `Dealer BUST! Player WON $${bet}!!`;
-    player_balance += 2 * bet;
-    document.getElementById("balance-amt").textContent = player_balance;
-    
-  } 
-  else if (player_total > dealer_total) {
-    document.getElementById("turn").textContent = `Player WON $${bet}!!`;
-    player_balance += 2 * bet;
-    document.getElementById("balance-amt").textContent = player_balance;
-  } else {
-    document.getElementById("turn").textContent = `Dealer WON $${bet}!!`;
+  // conditions for AI auto play
+  if (canAutoPlay()) {
+    setTimeout(() => {
+      replay();
+    }, 1000);
+
+    // setTimeout(() => {
+    //   placeBet();
+    // }, 1200);
+
+    if (game_count % 10 == 0) {
+      console.log(`After ${game_count} games`);
+      console.log(">> Wins: ", wins);
+      console.log(">> Player Balance: ", player_balance);
+    }
   }
-    
-  document.getElementById("turn-div").style.backgroundColor = "yellow";
-  document.getElementById("replay-btn").style.visibility = "visible";
-
-  // if(player_balance < 50){
-  //   document.getElementById("replay-btn").disabled = true;
-  // }
 }
 
 function allIn() {
   document.getElementById("bet-amt").value = player_balance;
 }
+
 function setBet(amt) {
   if (amt <= player_balance){
     document.getElementById("bet-amt").value = amt;
@@ -251,14 +330,14 @@ function setBet(amt) {
   else{
     document.getElementById("bet-amt").value = "";
     alert("Bet must be in the range of $50 and $" +player_balance);
+    return;
   }
 }
 
-// document.getElementById("dealer-hit").disabled = true; 
 function placeBet() {
-  bet = document.getElementById("bet-amt").value;
+  bet = Number(document.getElementById("bet-amt").value);
 
-  if( bet % 50 == 0 && bet >= 50 && bet <= player_balance){
+  if( bet % 50 == 0 && bet >= 50 && bet <= player_balance && player_balance >= 50){
     player_balance = player_balance - bet;
     document.getElementById("balance-amt").textContent = player_balance;
     document.getElementById("betted-amt").textContent = bet;
@@ -268,33 +347,40 @@ function placeBet() {
     form_box.style.display = "none";
     distribute_cards();
   }
+
   else{
     document.getElementById("bet-amt").value = "";
-    alert("1. bet must me in multiples of 50\n2. Bet must be in the range of $50 and $"+player_balance)
+    alert("1. bet must me in multiples of 50\n2. Bet must be in the range of $50 and $"+player_balance);
+    return;
   }
 
   if (player_type == "AI") {
-    while (player_total <= 15) {
-      // console.log("player hit");
-      hitPlayer();
-    }
-    if (player_total < 21) {
-      missPlayer();
-    }
+      while (player_total <= 12) {
+        hitPlayer();      // hit for sure
+      }
+
+      if(player_total > 17) {
+        missPlayer(); // miss for sure
+      } 
+
+      else if (player_total > 12 && player_total <= 17){
+        var choice = getRandomInt(0, 1);
+        while (player_total > 12 && player_total <= 17 && choice == 1) {
+          hitPlayer();
+        }
+        missPlayer();
+      }
+  
   }
 
-}
-
-if (player_total >= 21) {
-  document.getElementById("player-hit").disabled = true;
 }
 
 function replay() {
   if (player_balance < 50){
     document.getElementById("replay-btn").disabled = true;
     alert("Player is out of balance. Game over!!");
+    return;
   }else{
-    // window.location.reload("Refresh");
     selected = [];
     player_cards = [];
     dealer_cards = [];
@@ -316,23 +402,30 @@ function replay() {
     document.getElementById("player").style.backgroundColor = green;
     document.getElementById("player-hit").disabled = false;
     document.getElementById("player-miss").disabled = false;
+
+    if (canAutoPlay()) {
+      setTimeout(() => {
+        placeBet();
+      }, 1000);
+    }
   }
 }
 
 function selectPlayer(player_radio) {
   player_type = player_radio.value;
-  // console.log(player_radio.value);
   if (player_type == "AI") {
     document.getElementById("player-hit").disabled = true;
     document.getElementById("player-miss").disabled = true;
     document.getElementById("player-hit").style.display = "none";
     document.getElementById("player-miss").style.display = "none";
     document.getElementById("AI-player").style.display = "block";
+    document.getElementById("SpanAutoPlayAI").style.visibility = "visible";
   } else {
     document.getElementById("player-hit").disabled = false;
     document.getElementById("player-miss").disabled = false;
     document.getElementById("AI-player").style.display = "none";
     document.getElementById("player-hit").style.display = "inline";
     document.getElementById("player-miss").style.display = "inline";
+    document.getElementById("SpanAutoPlayAI").style.visibility = "hidden";
   }
 }
---
+++ xtra/tmp.js
@@ -0,0 +1,7 @@
+obj = {
+    [[1,1]] : 1,
+    [[1,2]] : 2,
+    [[1,3]] : 3,
+};
+
+console.log(JSON.stringify(obj))
